3dc374e39a20aa5a9198433a7715e62c90da4a4e
NickDarby77 <132682600+NickDarby77@users.noreply.github.com>
2023-05-26 03:54:56 +0600

    first commit

---
+++ bin/lesson4_homework.dart
@@ -0,0 +1,44 @@
+void main() {
+// task 1
+  List <int> numbers = [1, 7, 12, 3, 56, 2, 87, 34, 54];
+  print('${numbers.first}, ${numbers[4]}, ${numbers.last}');
+
+// task 2
+  /*List <int> list1 = [3, 12, 43, 1, 25, 6, 5, 7];
+  List <int> list2 = [55, 11, 23, 56, 78, 1, 9];
+  //print(list1 + list2);
+  list1.addAll(list2);
+  print(list1);*/
+
+// task 3
+  /*List <String> letters = ['a','d','F','l','u','t','t','e','R','y','3','b','h','j'];
+  print(letters.getRange(2, 9).toList());*/
+
+// task 4
+  /*List <int> nums = [1, 2, 3, 4, 5, 6, 7];
+  print(nums.contains(3));
+  print(('${nums.first}, ${nums.last}').length);
+  print('${nums[0]}, ${nums[6]}');
+  print(nums.length);*/
+
+// task 5
+  /*List list = [601, 123, 2, "dart", 45, 95, "dart24", 1];
+  print(list.contains('dart'));
+  print(list.contains(951));*/
+
+// task 6
+  /*List list3 = ['post', 1, 0, 'flutter'];
+  String myDart = 'Flutter';
+  print(list3.contains(myDart.toLowerCase()));*/
+
+// task 7
+  /*List list4 = ['I', 'Started', "Learn", "Flutter", "Since", "April"];
+  String myFlutter = '';
+  myFlutter = list4.join(' * ');
+  print(myFlutter);*/
+
+// task 8
+  /*List <int> digits = [1, 9, 3, 195, 202, 2, 5, 7, 9, 10, 3, 15, 0, 11];
+  digits.sort();
+  print(digits);*/
+}
